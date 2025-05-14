19da24d48aa490f3abbffe265c34ff31e561b6cf
hechth <helge.hecht@recetox.muni.cz>
2025-05-14 12:12:44 +0200

    styler format

--- runit/qualitymetrics_tests.R
+++ runit/qualitymetrics_tests.R
@@ -1,31 +1,31 @@
 test_input_default <- function() {
+  testDirC <- "input"
+  argLs <- list(
+    CV = "FALSE",
+    Compa = "TRUE",
+    seuil = 1,
+    poolAsPool1L = "TRUE"
+  )
 
-    testDirC <- "input"
-    argLs <- list(CV = "FALSE",
-                  Compa = "TRUE",
-                  seuil = 1,
-                  poolAsPool1L = "TRUE")
+  argLs <- c(defaultArgF(testDirC), argLs)
+  outLs <- wrapperCallF(argLs)
 
-    argLs <- c(defaultArgF(testDirC), argLs)
-    outLs <- wrapperCallF(argLs)
-
-    checkEqualsNumeric(outLs[["varDF"]]["met_033", "blankMean_over_sampleMean"], 0.004417387, tolerance = 1e-6)
- 
+  checkEqualsNumeric(outLs[["varDF"]]["met_033", "blankMean_over_sampleMean"], 0.004417387, tolerance = 1e-6)
 }
 
 test_formatOrder <- function() {
+  ## two first samples swapped in sampleMetadata
 
-    ## two first samples swapped in sampleMetadata
+  testDirC <- "formatOrder"
+  argLs <- list(
+    CV = "FALSE",
+    Compa = "TRUE",
+    seuil = 1,
+    poolAsPool1L = "TRUE"
+  )
 
-    testDirC <- "formatOrder"
-    argLs <- list(CV = "FALSE",
-                  Compa = "TRUE",
-                  seuil = 1,
-                  poolAsPool1L = "TRUE")
+  argLs <- c(defaultArgF(testDirC), argLs)
+  outLs <- wrapperCallF(argLs)
 
-    argLs <- c(defaultArgF(testDirC), argLs)
-    outLs <- wrapperCallF(argLs)
-
-    checkEqualsNumeric(outLs[["varDF"]]["met_033", "blankMean_over_sampleMean"], 0.004417387, tolerance = 1e-6)
- 
+  checkEqualsNumeric(outLs[["varDF"]]["met_033", "blankMean_over_sampleMean"], 0.004417387, tolerance = 1e-6)
 }
